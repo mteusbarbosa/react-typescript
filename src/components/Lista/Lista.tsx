ac51db0af9b86232fa001d393945bab932f3d916
mteusbarbosa <mateus.bm13@gmail.com>
2023-02-14 20:01:43 -0300

    1 file modified

--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,18 +1,28 @@
 import style from './Lista.module.scss';
 import Item from './Item/Item';
+import { useState } from 'react';
 
 export default function Lista() {
-  const itens = [
+  const [tarefas, setTarefas] = useState([
     { item: 'React', tempo: '00:02:00' },
     { item: 'Typescript', tempo: '01:00:00' },
-  ];
+  ]);
 
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTarefas([
+            ...tarefas,
+            { item: 'Estudar estado', tempo: '05:00:00' },
+          ]);
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
-        {itens.map((itens, index) => (
-          <Item key={index} {...itens} />
+        {tarefas.map((tarefas, index) => (
+          <Item key={index} {...tarefas} />
         ))}
       </ul>
     </aside>
